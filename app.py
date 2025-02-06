c832ab325d21b7509afc049440d2347ed8d7215e
selinal72 <selinal72@nycstudents.net>
2025-02-06 10:17:19 -0500

    tip calculator

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Strings are for representing characters, names, words etc.
+""" #Strings are for representing characters, names, words etc.
 name = "Deivid"
 #integer represents whole numbers
 age: 14
@@ -7,21 +7,31 @@ wallet = 5.45
 #boolean represents true or false, used in evaluations
 graduated = False
 
-""" def add(x,y):
+def add(x,y):
     print(x+y)
 #input asks the user a question and stores their response as a value
 bill = float(input("what was the bill"))
 #print (type(bill))
-#add(40,bill) """
+#add(40,bill)
 
 #lists
 students = ["Joanna", "Deivid", "David", "other David", "Ethan"]
 #similar to saying for i in range (5): print (students[i])
-""" print(students[4])
+print(students[4])
 for student in students:
-    print(student) """
+    print(student)
 moneys = [1,2,3,4,5,6]
 total = 0
 for money in moneys:
     total = total + money
-    print(total)
+    print(total) """
+
+# tip calculator
+def add(x,y):
+    print(x+y)
+bill = float(input("Bill"))
+tip = int(input("Tip"))
+print (type(bill))
+print (type(tip))
+total = add(bill,tip)
+print(f"Total: {total}")
