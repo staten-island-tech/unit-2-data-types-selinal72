3db277ed0013f4d7d3057650b93e120c16a93568
selinal72 <selinal72@nycstudents.net>
2025-02-07 10:16:33 -0500

    data challenge 1

--- app.py
+++ app.py
@@ -26,12 +26,28 @@ for money in moneys:
     total = total + money
     print(total) """
 
-# tip calculator
+""" # tip calculator
 def add(x,y):
-    print(x+y)
+    return(x+y) # return stores the code so it can be used wherever you need it
 bill = float(input("Bill"))
-tip = int(input("Tip"))
-print (type(bill))
-print (type(tip))
+tip = int(input("Tip")) # whole number
+print (type(bill)) # This prints the type of the bill to check if it's a float
+print (type(tip)) # This prints the type of the tip to check if it's an integer
 total = add(bill,tip)
-print(f"Total: {total}")
+print(f"Total: {total}") """
+
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+
+sentence = input("Enter Sentence: ") # asks user for input
+
+def count_words(sentence): # takes the sentence as input
+    words = sentence.split() # splits up words in the sentence
+    return len(words) # Return the number of words by using len() to count the list length
+
+print("Number of words: ", count_words(sentence)) # calls on the code
