ce62c76e63ecdc29edf10ad6d94ba5a68afd120d
selinal72 <selinal72@nycstudents.net>
2025-02-08 00:08:12 -0500

    using tip calculator

--- app.py
+++ app.py
@@ -26,15 +26,15 @@ for money in moneys:
     total = total + money
     print(total) """
 
-""" # tip calculator
+# tip calculator
 def add(x,y):
     return(x+y) # return stores the code so it can be used wherever you need it
-bill = float(input("Bill"))
-tip = int(input("Tip")) # whole number
+bill = float(input("Bill: "))
+tip = int(input("Tip: ")) # whole number
 print (type(bill)) # This prints the type of the bill to check if it's a float
 print (type(tip)) # This prints the type of the tip to check if it's an integer
 total = add(bill,tip)
-print(f"Total: {total}") """
+print(f"Total: {total}")
 
 
 """ x = "this is a thing"
@@ -44,10 +44,10 @@ print(y)
 print(z) """
 
 
-sentence = input("Enter Sentence: ") # asks user for input
+""" sentence = input("Enter Sentence: ") # asks user for input
 
 def count_words(sentence): # takes the sentence as input
     words = sentence.split() # splits up words in the sentence
     return len(words) # Return the number of words by using len() to count the list length
 
-print("Number of words: ", count_words(sentence)) # calls on the code
+print("Number of words: ", count_words(sentence)) # calls on the code """
